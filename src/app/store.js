2e99933ce8942f4c621cf48f845df532465d92f2
Elizabeth <elizabethriver@gmail.com>
2022-04-23 19:44:06 -0500

    update sore

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import  loginslide from './../view/login/loginslide';
-import registerSlice from '../view/register/registerSlice';
+import  loginslide from './../view/login/loginslide.js';
+import registerSlice from '../view/register/registerSlice.js';
 
 export const store = configureStore({
   reducer: {
