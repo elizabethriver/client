b7ef1b88fe4e4cf6c0d14140e10e031042eab5ca
Elizabeth <elizabethriver@gmail.com>
2022-04-30 12:58:00 -0500

    income Details

--- src/view/incomedetails/index.jsx
+++ src/view/incomedetails/index.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable no-useless-escape */
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import "./style/incomedetails.css";
-import EdiText from "react-editext";
-import { putIncomeByID, deleteIncomeByID } from "./../../api/api";
-import { getIncomeByIdTrunk } from "./incomeDetailsSlice";
+import { deleteIncomeByID } from "./../../api/api";
+import { getIncomeByIdTrunk, updateIncomeByIdTrunk } from "./incomeDetailsSlice";
 
 export const IncomeDetails = () => {
   const token = localStorage.getItem("token");
@@ -15,51 +14,53 @@ export const IncomeDetails = () => {
   const { dataIncomeById, loading } = useSelector(
     (state) => state.getIncomeByID
   );
-  const initFetch = useCallback(() => {
+  useEffect(() => {
     dispatch(getIncomeByIdTrunk({ token: token, incomeId: params.incomeId }));
   }, [dispatch, params.incomeId, token]);
 
-  useEffect(() => {
-    initFetch();
-  }, [initFetch]);
-
-  const [inputProduct, setInputProduct] = useState(dataIncomeById.product);
-  const [inputIncome, setInputIncome] = useState(dataIncomeById.income);
-
-  const handleSaveProduct = (product) => {
-    setInputProduct(product);
+  const [inputsEditMode, setInputsEditMode] = useState({
+    product: "",
+    income: "",
+  });
+  const [editState, setEditState] = useState(false);
+  const editMode = () => {
+    setEditState(!editState);
   };
-  const handleSaveIncome = (income) => {
-    setInputIncome(income);
+  const removeEditMode = () => {
+    setEditState(false);
   };
+  const onChangeHandlerInputs = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputsEditMode({
+      ...inputsEditMode,
+      [name]: value,
+    });
+  };
+
   const dataToUpdate = {
-    product: inputProduct,
-    income: parseInt(inputIncome),
+    product: inputsEditMode.product,
+    income: parseInt(inputsEditMode.income),
   };
 
   const submitUpdate = async (e) => {
     e.preventDefault();
     try {
-      const response = await putIncomeByID(
-        token,
-        dataToUpdate.product,
-        dataToUpdate.income,
-        params.incomeId
-      );
-      return response;
+      dispatch(
+        updateIncomeByIdTrunk({
+          token: token,
+          product: dataToUpdate.product,
+          income: dataToUpdate.income,
+          incomeId: params.incomeId,
+        })
+      ).unwrap();
+      removeEditMode()
+      navigate("/dashboard");
     } catch (error) {
       throw error;
     }
   };
-
-  const validationName = (val) => {
-    let regex = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
-    regex.test(val);
-  };
-  const validationNumber = (val) => {
-    let regex = /^[0-9]+$/;
-    regex.test(val);
-  };
+  
   const onClickDelete = async () => {
     console.log("Delete");
     try {
@@ -74,34 +75,47 @@ export const IncomeDetails = () => {
       throw error;
     }
   };
+
   if (loading) {
     return <p>Loading</p>;
   }
   return (
     <div>
       IncomeDetails {params.incomeId}
-      <form className="formForEdit" onSubmit={submitUpdate}>
-        <span>{dataIncomeById.product}</span>
-        <strong>{dataIncomeById.income}</strong>
-        <EdiText
-          validation={validationName}
-          validationMessage="Please type name income."
-          showButtonsOnHover
-          type="text"
-          value={inputProduct}
-          onSave={handleSaveProduct}
-        />
-        <EdiText
-          validation={validationNumber}
-          validationMessage="Please type income"
-          showButtonsOnHover
-          type="text"
-          value={inputIncome}
-          onSave={handleSaveIncome}
-        />
-        <button type="submit">Update</button>
-      </form>
-      <small id="mssgIncorrectTyping" />
+      <div>
+        {editState ? (
+          <form onSubmit={submitUpdate}>
+            <input
+              type="text"
+              name="product"
+              placeholder={dataIncomeById.product}
+              onChange={onChangeHandlerInputs}
+              value={inputsEditMode.product}
+              required
+              pattern="^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$"
+              title="Just type letters is allowed"
+            />
+            <input
+              type="text"
+              name="income"
+              placeholder={dataIncomeById.income}
+              onChange={onChangeHandlerInputs}
+              value={inputsEditMode.income}
+              required
+              pattern="^[0-9]+$"
+              title="Just type number is allowed"
+            />
+            <button type="submit">Save</button>
+            <button onClick={removeEditMode}>Cancel</button>
+          </form>
+        ) : (
+          <div>
+            <span>{dataIncomeById.product}</span>
+            <strong>${dataIncomeById.income}</strong>
+            <button onClick={editMode}>Update</button>
+          </div>
+        )}
+      </div>
       <button onClick={onClickDelete}>Delete</button>
     </div>
   );
