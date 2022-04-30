f24b3b683958b8412fa47d70f77897c0aa5e4343
Elizabeth <elizabethriver@gmail.com>
2022-04-30 17:04:14 -0500

    update expense by ID

--- src/view/expensedetails/expenseDetailsSlice.js
+++ src/view/expensedetails/expenseDetailsSlice.js
@@ -1,18 +1,36 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { getExpenseByID } from "../../api/api";
+import { getExpenseByID, putExpenseByID } from "../../api/api";
 const initialState = {
   dataExpenseById: [],
   loading: false,
+  docUpdateById: [],
 };
 
-export const getExpenseByIdTrunk = createAsyncThunk("getExpenseById/api",
+export const getExpenseByIdTrunk = createAsyncThunk(
+  "getExpenseById/api",
   async (dataExpenseByID) => {
     const { token, expenseId } = dataExpenseByID;
     try {
       const response = await getExpenseByID(token, expenseId);
-      const {findedObject} = response
+      const { findedObject } = response;
       return findedObject;
     } catch (error) {
+      throw error;
+    }
+  }
+);
+
+export const updateExpenseByIdTrunk = createAsyncThunk(
+  "updateExpenseByIdTrunk/api",
+  async (dataExpenseByID) => {
+    const { token, product, expense, expenseId } = dataExpenseByID;
+    try {
+      const response = await putExpenseByID(token, product, expense, expenseId);
+      console.log(response.data);
+      const { docUpdate } = response.data;
+      return docUpdate;
+    } catch (error) {
+      console.log(error)
       throw error;
     }
   }
@@ -36,6 +54,22 @@ const expenseDetailsSlice = createSlice({
     });
     // Add reducers for additional action types here, and handle loading state as needed
     builder.addCase(getExpenseByIdTrunk.rejected, (state) => {
+      // Add user to the state array
+      state.loading = false;
+    });
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(updateExpenseByIdTrunk.pending, (state) => {
+      // Add user to the state array
+      state.loading = true;
+    });
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(updateExpenseByIdTrunk.fulfilled, (state, action) => {
+      // Add user to the state array
+      state.loading = false;
+      state.docUpdateById = action.payload;
+    });
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(updateExpenseByIdTrunk.rejected, (state) => {
       // Add user to the state array
       state.loading = false;
     });
