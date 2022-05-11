656405438ad151b83b2e9a4a9032214c44bb34b6
Elizabeth <elizabethriver@gmail.com>
2022-05-11 17:26:28 -0500

    styling

--- src/components/cardStandard/cardStandard.jsx
+++ src/components/cardStandard/cardStandard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { productObject } from "../../utils/utils";
 import { Button } from "../buttons/button";
-import './style/cardStandard.css'
+import "./style/cardStandard.css";
 
 export const CardStandardProduct = ({
   name,
@@ -10,12 +10,19 @@ export const CardStandardProduct = ({
   numberData,
 }) => {
   let object = productObject(name);
-  const priceProduct = new Intl.NumberFormat().format(numberData)
+  const priceProduct = new Intl.NumberFormat().format(numberData);
   return (
     <section className="cardStandard">
       <span>{(object.product = productData)}</span>
-      <strong className="price_product">${(object[name] = priceProduct)}</strong>
-      <Button name='update_product' type='button' onClick={editMode} children="Update" />
+      <strong className="price_product">
+        ${(object[name] = priceProduct)}
+      </strong>
+      <Button
+        name="update_product"
+        type="button"
+        onClick={editMode}
+        children="Update"
+      />
     </section>
   );
 };
--- src/components/item/items.jsx
+++ src/components/item/items.jsx
@@ -7,28 +7,28 @@ export const Items = (props) => {
   return (
     <>
       {props?.array.map((item) => {
-        const priceIncome = new Intl.NumberFormat().format(item.income)
-        const priceExpense = new Intl.NumberFormat().format(item.expense)
+        const priceIncome = new Intl.NumberFormat().format(item.income);
+        const priceExpense = new Intl.NumberFormat().format(item.expense);
         return (
           <li className="container_list" id={`${item._id}`} key={item._id}>
-          <Button
-            name="details_link"
-            type="button"
-            children={
-              <LinkStandard
-                to={`${props.url}/${item._id}`}
-                children={<span className="item_product">{item.product}</span>}
-              />
-            }
-          />
-          <strong className="item_price">
-            {/* ${item.income ? item.income : item.expense} */}
-            ${isNaN(priceIncome) ? priceExpense : priceIncome }
-          </strong>
-        </li>
-        )
-      }
-      )}
+            <Button
+              name="details_link"
+              type="button"
+              children={
+                <LinkStandard
+                  to={`${props.url}/${item._id}`}
+                  children={
+                    <span className="item_product">{item.product}</span>
+                  }
+                />
+              }
+            />
+            <strong className="item_price">
+              {isNaN(priceIncome) ? priceExpense : priceIncome}
+            </strong>
+          </li>
+        );
+      })}
     </>
   );
 };
